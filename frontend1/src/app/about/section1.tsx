9379ad78e5598689823ae84a1d7b1fe1f9592127
abhijitacharya793 <abhijitacharya793@gmail.com>
2024-01-24 18:58:38 +0530

    Some data added

--- frontend1/src/app/about/section1.tsx
+++ frontend1/src/app/about/section1.tsx
@@ -7,19 +7,32 @@ export function Section1() {
       <div className="lg:max-w-screen-lg sm:max-w-screen-sm mx-auto flex items-center ">
         <div className="w-1/2 py-10">
           <div>
-            <p className="w-20 p-1 border-t border-b border-yellow-600 text-sm font-light">
-              ABOUT US
+            <p className="w-24 p-1 border-t border-b border-yellow-600 text-sm font-light">
+              CEO Speaks
             </p>
             <p className="app-header text-3xl font-bold pt-3">
-              Quality and Tradition
+              Quality and Experience
             </p>
             <p className="pt-3 text-sm text-app-gray">
-              Lorem Ipsum is that it has a more-or-less normal distribution of
-              letters, as opposed to using Content here, content gfshere making
-              look like readable English. Many desktop publishing packages.
+              Our CEO, Mr. Sunil Luthra is incredibly passionate about our
+              restaurants and the F&B Industry. He envisions it as a vibrant and
+              welcoming space where people can gather to enjoy delicious food
+              and create lasting memories. He believes in the power of
+              exceptional customer service and aim to provide a top-notch dining
+              experience for every guest. 
+              <br/>
+              The CEO is committed to using fresh,
+              locally sourced ingredients to create innovative and mouthwatering
+              dishes that cater to various dietary preferences. He is dedicated
+              to fostering a positive work culture, ensuring that our team is
+              motivated, well-trained, and shares the same passion for
+              delivering exceptional hospitality. Overall, our CEO's vision for
+              the restaurant is to be a place where people can indulge in
+              outstanding culinary experiences while feeling like they are part
+              of a larger community.
             </p>
 
-            <p className="pt-2">JOSEFINE</p>
+            <p className="pt-2">Sunil Luthra</p>
             <Image
               src="/sign.svg"
               width="100"
