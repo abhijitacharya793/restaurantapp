d366cd7588f3d8d9b7843670ea45da454a6e4c7d
abhijitacharya793 <abhijitacharya793@gmail.com>
2024-02-11 15:57:58 +0530

    Home section 4 responsive

--- frontend1/src/app/home/section4.tsx
+++ frontend1/src/app/home/section4.tsx
@@ -1,47 +1,48 @@
 "use client";
 import Image from "next/image";
 
-import { Variants, motion, useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export function Section4() {
-  const testimonialVariant: Variants = {
-    hide: {
-      opacity: 0,
-      x: 500,
+  const slides = [
+    {
+      name: "Daniyal Sppra",
+      image: "/testimonial1.svg",
+      location: "",
+      testimonial:
+        "&quot;It is professional, considers everyone&apos;s time, can think about the There are many variations of passages whole probls small niche, friendly.&quot;",
     },
-    show: {
-      opacity: 1,
-      x: 0,
-      transition: {
-        duration: 0.5,
-      },
+    {
+      name: "Natasha D",
+      image: "/testimonial2.svg",
+      location: "",
+      testimonial:
+        "&quot;It is professional, considers everyone&apos;s time, can think about the There are many variations of passages whole probls small niche, friendly.&quot;",
     },
+    {
+      name: "Jack Sparrow",
+      image: "/testimonial3.svg",
+      location: "",
+      testimonial:
+        "&quot;It is professional, considers everyone&apos;s time, can think about the There are many variations of passages whole probls small niche, friendly.&quot;",
+    },
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: (window.innerWidth < 768 && 1) || 3,
+    slidesToScroll: 1,
   };
 
-  return (
-    <>
-      <div className="lg:max-w-screen-lg mx-auto py-20">
-        <div>
-          <p className="w-24 p-1 border-t border-b border-yellow-600 text-white text-sm font-light ">
-            TESTIMONIAL
-          </p>
-        </div>
-        <p className="app-header text-3xl font-bold pb-3 text-white pt-4">
-          What our clients say
-        </p>
-        <p className="pb-4 text-white">
-          We love to hear from customers, so please leave a comment or say hello
-          in an email.
-        </p>
-        <motion.div
-          initial="hide"
-          whileInView="show"
-          exit="hide"
-          variants={testimonialVariant}
-          className="flex items-center"
-        >
-          <div className="w-1/3 mr-4">
+  const SimpleSlider = ({ slides }) => {
+    return (
+      <Slider {...settings}>
+        {slides.map((slide, index) => (
+          <div key={index} className="lg:w-1/3 mr-4 px-2">
             <div className="bg-app-dark-purple overflow-hidden p-3">
               <div className="p-4">
                 <div className="flex items-center">
@@ -49,12 +50,12 @@ export function Section4() {
                     width={100}
                     height={100}
                     className="w-24 h-24 rounded-full mr-4"
-                    src="/testimonial1.svg"
+                    src={slide.image}
                     alt="Avatar"
                   />
                   <div>
                     <h2 className="font-semibold text-lg text-white">
-                      Daniyal Sppra
+                      {slide.name}
                     </h2>
                     <p className="text-app-yellow">Newyork</p>
                   </div>
@@ -70,68 +71,29 @@ export function Section4() {
               </div>
             </div>
           </div>
-          <div className="w-1/3 mr-4">
-            <div className="bg-app-dark-purple overflow-hidden p-3">
-              <div className="p-4">
-                <div className="flex items-center">
-                  <Image
-                    width={100}
-                    height={100}
-                    className="w-24 h-24 rounded-full mr-4"
-                    src="/testimonial2.svg"
-                    alt="Avatar"
-                  />
-                  <div>
-                    <h2 className="font-semibold text-lg text-white">
-                      Natasha D
-                    </h2>
-                    <p className="text-app-yellow">Salt Lake City</p>
-                  </div>
-                </div>
-                <hr className="border border-slate-500 my-3" />
-                <div className="mt-4">
-                  <p className="app-header text-white italic">
-                    &quot;It is professional, considers everyone&apos;s time,
-                    can think about the There are many variations of
-                    passages.&quot;
-                    <br />
-                    <br />
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="w-1/3">
-            <div className="bg-app-dark-purple overflow-hidden p-3">
-              <div className="p-4">
-                <div className="flex items-center">
-                  <Image
-                    width={100}
-                    height={100}
-                    className="w-24 h-24 rounded-full mr-4"
-                    src="/testimonial3.svg"
-                    alt="Avatar"
-                  />
-                  <div>
-                    <h2 className="font-semibold text-lg text-white">
-                      Jack Sparrow
-                    </h2>
-                    <p className="text-app-yellow">San Diego</p>
-                  </div>
-                </div>
-                <hr className="border border-slate-500 my-3" />
-                <div className="mt-4">
-                  <p className="app-header text-white italic">
-                    &quot;It is professional, considers everyone&apos;s time,
-                    can think about the There are many passages whole probls
-                    small niche, friendly.&quot;
-                    <br />
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </motion.div>
+        ))}
+      </Slider>
+    );
+  };
+
+  return (
+    <>
+      <div className="lg:max-w-screen-lg mx-auto py-20 px-8 lg:px-0">
+        <div>
+          <p className="w-24 p-1 border-t border-b border-yellow-600 text-white text-sm font-light ">
+            TESTIMONIAL
+          </p>
+        </div>
+        <p className="app-header text-3xl font-bold pb-3 text-white pt-4">
+          What our clients say
+        </p>
+        <p className="pb-4 text-white">
+          We love to hear from customers, so please leave a comment or say hello
+          in an email.
+        </p>
+        <div className="">
+          <SimpleSlider slides={slides} />
+        </div>
       </div>
     </>
   );
