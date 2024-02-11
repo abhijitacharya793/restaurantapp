99463f19de361f9495a9e24cea3501c81cd22483
abhijitacharya793 <abhijitacharya793@gmail.com>
2024-02-11 20:34:58 +0530

    Responsive gallery

--- frontend1/src/app/gallery/section1.tsx
+++ frontend1/src/app/gallery/section1.tsx
@@ -6,26 +6,26 @@ export function Section1() {
     <>
       <div className="lg:max-w-screen-lg items-center mx-4 lg:mx-auto">
         {/* ROW 1 */}
-        <div className="flex w-full py-4">
-          <div className="w-1/4 px-2">
+        <div className="lg:flex w-full py-4">
+          <div className="lg:w-1/4 px-4 py-2 lg:px-2 lg:py-0">
             <Image
               src="/gallery1.jpg"
               alt="home"
               width={450}
               height={700}
-              className="h-96"
+              className="h-96 w-auto"
             />
           </div>
-          <div className="w-1/4 px-2">
+          <div className="lg:w-1/4 px-4 py-2 lg:px-2 lg:py-0">
             <Image
               src="/gallery2.jpg"
               alt="home"
               width={450}
               height={700}
-              className="h-96"
+              className="h-96 w-auto"
             />
           </div>
-          <div className="w-1/2 px-2">
+          <div className="lg:w-1/2 px-4 py-2 lg:px-2 lg:py-0">
             <Image
               src="/popular3.jpg"
               alt="home"
@@ -36,8 +36,8 @@ export function Section1() {
           </div>
         </div>
         {/* ROW 2 */}
-        <div className="flex w-full py-4">
-          <div className="w-1/2 px-2">
+        <div className="lg:flex w-full py-4">
+          <div className="lg:w-1/2 px-4 py-2 lg:px-2 lg:py-0">
             <Image
               src="/popular3.jpg"
               alt="home"
@@ -46,22 +46,22 @@ export function Section1() {
               className="h-96 w-auto"
             />
           </div>
-          <div className="w-1/4 px-2">
+          <div className="lg:w-1/4 px-4 py-2 lg:px-2 lg:py-0">
             <Image
               src="/gallery1.jpg"
               alt="home"
               width={450}
               height={700}
-              className="h-96"
+              className="h-96 w-auto"
             />
           </div>
-          <div className="w-1/4 px-2">
+          <div className="lg:w-1/4 px-4 py-2 lg:px-2 lg:py-0">
             <Image
               src="/gallery2.jpg"
               alt="home"
               width={450}
               height={700}
-              className="h-96"
+              className="h-96 w-auto"
             />
           </div>
         </div>
