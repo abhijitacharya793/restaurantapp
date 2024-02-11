a111ad71345f83bbfa64b9a5ef69d1be880af614
abhijitacharya793 <abhijitacharya793@gmail.com>
2024-02-11 12:50:44 +0530

    Header responsive

--- frontend1/src/components/Header.tsx
+++ frontend1/src/components/Header.tsx
@@ -1,28 +1,14 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import { AppButton } from "./AppButton";
 import Image from "next/image";
-
-import { motion } from "framer-motion";
+import { Navbar } from "./Navbar";
 
 type HeaderProps = {
   active_page: string;
 };
 
-const links = [
-  { href: "/home", text: "home", margin: "mr-8" },
-  { href: "/about", text: "about", margin: "mr-8" },
-  { href: "/menu", text: "menu", margin: "mr-8" },
-  { href: "/reservation", text: "reservation", margin: "mr-12" },
-  { href: "/gallery", text: "gallery", margin: "mr-8" },
-  { href: "/blog", text: "blog", margin: "mr-4" },
-  { href: "/contact", text: "contact", margin: "mr-8" },
-];
-
 export function Header({ active_page }: HeaderProps) {
-  const path = usePathname();
-
   return (
     <>
       {/* Header top */}
@@ -62,65 +48,10 @@ export function Header({ active_page }: HeaderProps) {
       </div>
       <hr className="border-slate-400" />
       {/* Header nav */}
-      <div className="lg:max-w-screen-lg sm:max-w-screen-sm mx-auto flex text-left py-2">
-        {/* space */}
-
-        {/* 7 parts of 12 */}
-        {links.map((link) => (
-          <motion.div
-            key={link.text}
-            whileHover={{ scale: 1.1 }}
-            className={`w-1/12 h-10 py-2 text-white capitalize ${link.margin}`}
-          >
-            <a
-              href={link.href}
-              className={`${
-                link.href === path ? "font-bold text-app-yellow" : ""
-              } hover:text-app-yellow`}
-            >
-              {link.text}
-            </a>
-          </motion.div>
-        ))}
-        {/* space */}
-        <div className="w-1/3 h-10 py-2"></div>
-        {/* socials */}
-        <motion.div
-          whileHover={{ scale: 1.1 }}
-          className="flex w-1/12 h-10 py-2 justify-end"
-        >
-          <Image
-            alt=""
-            width={100}
-            height={100}
-            src="/insta.svg"
-            className="h-5 w-5 mx-2"
-          />
-          <Image
-            alt=""
-            width={100}
-            height={100}
-            src="/facebook.svg"
-            className="h-5 w-5 mx-2"
-          />
-          <Image
-            alt=""
-            width={100}
-            height={100}
-            src="https://assets-global.website-files.com/622856f73bdd0f82f7741cb2/655afe9f73021a4d4bc26a34_social-white-icon-3.svg"
-            className="h-5 w-5 mx-2"
-          />
-          <Image
-            alt=""
-            width={100}
-            height={100}
-            src="/pintrest.svg"
-            className="h-5 w-5 mx-2"
-          />
-        </motion.div>
-        {/* space */}
-      </div>
+      <Navbar />
+      
       <hr className="border-slate-400" />
     </>
   );
 }
+
---
+++ frontend1/src/components/Navbar.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { Bars3Icon } from "@heroicons/react/20/solid";
+
+export function Navbar() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  const links = [
+    { href: "/home", text: "home", margin: "mr-8" },
+    { href: "/about", text: "about", margin: "mr-8" },
+    { href: "/menu", text: "menu", margin: "mr-8" },
+    { href: "/reservation", text: "reservation", margin: "mr-12" },
+    { href: "/gallery", text: "gallery", margin: "mr-8" },
+    { href: "/blog", text: "blog", margin: "mr-4" },
+    { href: "/contact", text: "contact", margin: "mr-8" },
+  ];
+  const path = usePathname();
+  return (
+    <>
+      <div className="lg:max-w-screen-lg sm:max-w-screen-sm mx-auto flex text-left py-2">
+        {/* Hamburger Menu Icon */}
+        <div
+          className="block lg:hidden cursor-pointer items-center "
+          onClick={toggleMenu}
+        >
+          <Bars3Icon className="w-6 h-6 text-white mx-6 my-2" />
+        </div>
+
+        {/* Navigation Links */}
+        <div
+          className={`lg:flex lg:items-center w-full lg:w-auto ${
+            isMenuOpen ? "block" : "hidden"
+          }`}
+        >
+          {/* 7 parts of 12 */}
+          {links.map((link) => (
+            <motion.div
+              key={link.text}
+              whileHover={{ scale: 1.1 }}
+              className={`w-full sm:w-1/2 md:w-1/4 lg:w-1/12 h-10 py-2 text-white capitalize ${link.margin}`}
+            >
+              <a
+                href={link.href}
+                className={`${
+                  link.href === path ? "font-bold text-app-yellow" : ""
+                } hover:text-app-yellow`}
+              >
+                {link.text}
+              </a>
+            </motion.div>
+          ))}
+          {/* space */}
+
+          {/* space */}
+        </div>
+        <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/3 h-10 py-2"></div>
+        {/* socials */}
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          className="flex w-full sm:w-1/2 md:w-1/4 lg:w-1/12 h-10 py-2 justify-end"
+        >
+          <Image
+            alt=""
+            width={100}
+            height={100}
+            src="/insta.svg"
+            className="h-5 w-5 mx-2"
+          />
+          <Image
+            alt=""
+            width={100}
+            height={100}
+            src="/facebook.svg"
+            className="h-5 w-5 mx-2"
+          />
+          <Image
+            alt=""
+            width={100}
+            height={100}
+            src="https://assets-global.website-files.com/622856f73bdd0f82f7741cb2/655afe9f73021a4d4bc26a34_social-white-icon-3.svg"
+            className="h-5 w-5 mx-2"
+          />
+          <Image
+            alt=""
+            width={100}
+            height={100}
+            src="/pintrest.svg"
+            className="h-5 w-5 mx-2"
+          />
+        </motion.div>
+      </div>
+    </>
+  );
+}
