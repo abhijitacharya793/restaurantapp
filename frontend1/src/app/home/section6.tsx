91556ea52296dd504c8a0e71639388e634da6213
abhijitacharya793 <abhijitacharya793@gmail.com>
2024-02-11 19:29:24 +0530

    Responsive about

--- frontend1/src/app/home/section6.tsx
+++ frontend1/src/app/home/section6.tsx
@@ -1,7 +1,84 @@
+"use client";
 import { AppButton } from "@/components/AppButton";
 import Image from "next/image";
 
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { useEffect, useState } from "react";
+
 export function Section6() {
+  type Slide = {
+    name: string;
+    image: string;
+    description: string;
+    price: string;
+  };
+  const slides: Slide[] = [
+    {
+      name: "Chicken Manjoori",
+      image: "/popular1.svg",
+      description: "Lorem Ipsum is that it has a more-or-less normal",
+      price: "15",
+    },
+    {
+      name: "Chicken Manjoori",
+      image: "/popular2.svg",
+      description: "Lorem Ipsum is that it has a more-or-less normal",
+      price: "15",
+    },
+    {
+      name: "Chicken Manjoori",
+      image: "/popular3.svg",
+      description: "Lorem Ipsum is that it has a more-or-less normal",
+      price: "15",
+    },
+    {
+      name: "Chicken Manjoori",
+      image: "/popular4.svg",
+      description: "Lorem Ipsum is that it has a more-or-less normal",
+      price: "15",
+    },
+  ];
+
+  const [slidesCount, setSlidesCount] = useState(1);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: slidesCount,
+    slidesToScroll: 1,
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setSlidesCount((window.innerWidth < 767 && 1) || 4);
+    }
+  }, []);
+  const SimpleSlider = () => {
+    return (
+      <Slider {...settings}>
+        {slides.map((slide, index) => (
+          <div key={index} className="px-2">
+            <Image
+              alt=""
+              width={300}
+              height={300}
+              src={slide.image}
+              className="flex justify-center"
+            />
+            <div className="flex my-2 app-header font-bold text-xl">
+              <p className="w-full">{slide.name}</p>
+              <p>${slide.price}</p>
+            </div>
+            <p className="text-sm text-app-gray">{slide.description}</p>
+          </div>
+        ))}
+      </Slider>
+    );
+  };
+
   return (
     <>
       <div className="lg:max-w-screen-lg mx-auto pb-10">
@@ -17,74 +94,8 @@ export function Section6() {
             letters, as opposed to using Content here, content making.
           </p>
         </div>
-        <div className="lg:flex px-4 lg:px-0">
-          <div className="lg:w-1/4 lg:mr-4 mb-8 lg:mb-0">
-            <Image
-              alt=""
-              width={300}
-              height={300}
-              src="/popular1.svg"
-              className=""
-            />
-            <div className="flex my-2 app-header font-bold text-xl">
-              <p className="w-full">Chicken Manjoori</p>
-              <p>$15</p>
-            </div>
-            <p className="text-sm text-app-gray">
-              Lorem Ipsum is that it has a more-or-less normal
-            </p>
-          </div>
-          <hr className="border border-slate-500  show_mobile" />
-          <div className="lg:w-1/4 lg:mr-4 my-8 lg:my-0">
-            <Image
-              alt=""
-              width={300}
-              height={300}
-              src="/popular2.svg"
-              className="flex justify-center"
-            />
-            <div className="flex my-2 app-header font-bold text-xl">
-              <p className="w-full">Chicken Manjoori</p>
-              <p>$15</p>
-            </div>
-            <p className="text-sm text-app-gray">
-              Lorem Ipsum is that it has a more-or-less normal
-            </p>
-          </div>
-          <hr className="border border-slate-500  show_mobile" />
-          <div className="lg:w-1/4 lg:mr-4 my-8 lg:my-0">
-            <Image
-              alt=""
-              width={300}
-              height={300}
-              src="/popular3.svg"
-              className="flex justify-center"
-            />
-            <div className="flex my-2 app-header font-bold text-xl">
-              <p className="w-full">Chicken Manjoori</p>
-              <p>$15</p>
-            </div>
-            <p className="text-sm text-app-gray">
-              Lorem Ipsum is that it has a more-or-less normal
-            </p>
-          </div>
-          <hr className="border border-slate-500  show_mobile" />
-          <div className="lg:w-1/4 mt-8 lg:mt-0">
-            <Image
-              alt=""
-              width={300}
-              height={300}
-              src="/popular4.svg"
-              className="flex justify-center"
-            />
-            <div className="flex my-2 app-header font-bold text-xl">
-              <p className="w-full">Chicken Manjoori</p>
-              <p>$15</p>
-            </div>
-            <p className="text-sm text-app-gray">
-              Lorem Ipsum is that it has a more-or-less normal
-            </p>
-          </div>
+        <div className="px-4 lg:px-0">
+          <SimpleSlider />
         </div>
         <div className="flex items-center justify-center pt-10">
           <AppButton
