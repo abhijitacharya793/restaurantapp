bbc851163018df37389b20ec62eaf9e55462b758
abhijitacharya793 <abhijitacharya793@gmail.com>
2024-02-11 13:52:39 +0530

    Home section 2 responsive

--- frontend1/src/app/home/section2.tsx
+++ frontend1/src/app/home/section2.tsx
@@ -17,13 +17,13 @@ export function Section2() {
         transition={{ delay: 0.5 }}
       >
         {/* P1 */}
-        <div className="lg:max-w-screen-lg mx-auto lg:flex items-center pt-32  ">
-          <div className="w-1/3 lg:justify-start">
-            <div className="flex text-left">
-              <div className="w-1/6 h-16 flex items-center justify-start">
+        <div className="lg:max-w-screen-lg mx-auto lg:flex items-center pt-4 lg:pt-32 pl-10 lg:pl-0">
+          <div className="lg:w-1/3 lg:justify-start">
+            <div className="flex text-left my-4">
+              <div className="lg:w-1/6 lg:h-16 flex lg:items-center lg:justify-start">
                 <MapPinIcon className="w-12 h-12 p-2 bg-black text-white rounded-full" />
               </div>
-              <div className="w-5/6 h-16 p-2">
+              <div className="w-5/6 lg:h-16 pl-2 mx-10 lg:mx-4">
                 <p className="text-xl app-header font-bold">Locate Us</p>
                 <p className="text-xs text-app-gray">
                   1st Floor, City Centre Mall
@@ -36,12 +36,12 @@ export function Section2() {
               </div>
             </div>
           </div>
-          <div className="w-1/3 lg:justify-center">
-            <div className="flex text-left">
-              <div className="w-2/6 h-16 flex items-center justify-end">
+          <div className="lg:w-1/3 lg:justify-center">
+            <div className="flex text-left my-4">
+              <div className="lg:w-2/6 lg:h-16 flex lg:items-center lg:justify-end">
                 <ClockIcon className="w-12 h-12 p-2 bg-black text-white rounded-full" />
               </div>
-              <div className="w-4/6 h-16 p-2">
+              <div className="w-4/6 lg:h-16 pl-2 mx-10 lg:mx-4">
                 <p className="text-lg app-header font-bold">Open Hours</p>
                 <p className="text-xs text-app-gray">
                   <span className=" font-bold">Mon to Fri: </span>12:00 pm to
@@ -53,12 +53,12 @@ export function Section2() {
               </div>
             </div>
           </div>
-          <div className="w-1/3 lg:justify-end">
-            <div className="flex text-left">
-              <div className="w-3/6 h-16 flex items-center justify-end">
+          <div className="lg:w-1/3 lg:justify-end">
+            <div className="flex text-left my-4">
+              <div className="lg:w-3/6 lg:h-16 flex lg:items-center lg:justify-end">
                 <PencilSquareIcon className="w-12 h-12 p-2 bg-black text-white rounded-full" />
               </div>
-              <div className="w-3/6 h-16 p-2">
+              <div className="w-3/6 lg:h-16 pl-2 mx-10 lg:mx-4">
                 <p className="text-lg app-header font-bold">Reservation</p>
                 <p className="text-xs text-app-gray">
                   hirestaurantate@gmail.com
@@ -69,7 +69,7 @@ export function Section2() {
         </div>
         {/* P2 */}
         <div className="lg:max-w-screen-lg mx-auto flex pt-10 pb-20">
-          <div className="w-1/2 border-r border-gray-300 pr-4">
+          <div className="w-1/2 border-r border-gray-300 pr-4 hide_img_mobile">
             <div className="flex w-full items-center justify-left">
               <Image
                 src="/story1.jpg"
@@ -80,7 +80,7 @@ export function Section2() {
               />
             </div>
           </div>
-          <div className="w-1/2 ml-8">
+          <div className="lg:w-1/2 ml-8 mr-10 lg:mr-0">
             <h1 className="app-header text-3xl font-bold pb-2">The Story</h1>
             <p className="text-xs text-app-gray">
               Lorem Ipsum is that it has a more-or-less normal distribution of
@@ -103,13 +103,13 @@ export function Section2() {
                 </p>
               </div>
             </div>
-            <p className="pt-8">JOSEFINE</p>
+            <p className="pt-8">Sunil Luthra</p>
             <Image
               src="/sign.svg"
               width="100"
               height="100"
               alt="home"
-              className="pt-2"
+              className="pt-2 "
             />
           </div>
         </div>
