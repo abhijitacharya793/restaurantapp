a730054358347ea54ff2f6607448df95ded2132d
abhijitacharya793 <abhijitacharya793@gmail.com>
2024-09-08 11:33:57 +0530

    fe2

--- frontend1/src/app/layout.tsx
+++ frontend1/src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "frontend1",
+  title: "frontend2",
   description: "Created by @katsuro",
 };
 
