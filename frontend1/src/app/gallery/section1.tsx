990cb499cdff9e9504530aa5e859f5225c5bc37c
abhijitacharya793 <abhijitacharya793@gmail.com>
2024-11-10 16:08:05 +0530

    gallery

--- frontend1/src/app/gallery/section1.tsx
+++ frontend1/src/app/gallery/section1.tsx
@@ -13,7 +13,7 @@ export function Section1() {
               alt="home"
               width={450}
               height={700}
-              className="lg:h-96 lg:w-auto w-full"
+              className="lg:h-96 lg:w-auto w-full object-cover"
             />
           </div>
           <div className="lg:w-1/4 px-4 py-2 lg:px-2 lg:py-0">
@@ -22,16 +22,16 @@ export function Section1() {
               alt="home"
               width={450}
               height={700}
-              className="lg:h-96 lg:w-auto w-full"
+              className="lg:h-96 lg:w-auto w-full object-cover"
             />
           </div>
           <div className="lg:w-1/2 px-4 py-2 lg:px-2 lg:py-0">
             <Image
-              src="/popular3.jpg"
+              src="/menu_item_Afghani chicken.jpg"
               alt="home"
               width={450}
               height={700}
-              className="lg:h-96 lg:w-auto w-full"
+              className="lg:h-96 lg:w-auto w-full object-cover"
             />
           </div>
         </div>
@@ -39,29 +39,80 @@ export function Section1() {
         <div className="lg:flex w-full py-4">
           <div className="lg:w-1/2 px-4 py-2 lg:px-2 lg:py-0">
             <Image
-              src="/popular3.jpg"
+              src="/menu_item_Veg biryani.jpg"
               alt="home"
               width={450}
               height={700}
-              className="lg:h-96 lg:w-auto w-full"
+              className="lg:h-96 lg:w-auto w-full object-cover"
             />
           </div>
           <div className="lg:w-1/4 px-4 py-2 lg:px-2 lg:py-0">
             <Image
-              src="/gallery1.jpg"
+              src="/menu_item_Stir fried vegetables.jpg"
               alt="home"
               width={450}
               height={700}
-              className="lg:h-96 lg:w-auto w-full"
+              className="lg:h-96 lg:w-auto w-full object-cover"
             />
           </div>
           <div className="lg:w-1/4 px-4 py-2 lg:px-2 lg:py-0">
             <Image
-              src="/gallery2.jpg"
+              src="/menu_item_Cucumber cola.jpg"
               alt="home"
               width={450}
               height={700}
-              className="lg:h-96 lg:w-auto w-full"
+              className="lg:h-96 lg:w-auto w-full object-cover"
+            />
+          </div>
+        </div>
+        {/* ROW 3 */}
+        <div className="lg:flex w-full py-4">
+          <div className="lg:w-1/2 px-4 py-2 lg:px-2 lg:py-0">
+            <Image
+              src="/menu_item_Golden fried prawns.jpg"
+              alt="home"
+              width={450}
+              height={700}
+              className="lg:h-96 lg:w-auto w-full object-cover"
+            />
+          </div>
+          <div className="lg:w-1/2 px-4 py-2 lg:px-2 lg:py-0">
+            <Image
+              src="/menu_item_Fresh and fruity.jpg"
+              alt="home"
+              width={450}
+              height={700}
+              className="lg:h-96 lg:w-auto w-full object-cover"
+            />
+          </div>
+        </div>
+        {/* ROW 4 */}
+        <div className="lg:flex w-full py-4">
+          <div className="lg:w-1/2 px-4 py-2 lg:px-2 lg:py-0">
+            <Image
+              src="/menu_item_Paneer malai tikka.jpg"
+              alt="home"
+              width={450}
+              height={700}
+              className="lg:h-96 lg:w-auto w-full object-cover"
+            />
+          </div>
+          <div className="lg:w-1/4 px-4 py-2 lg:px-2 lg:py-0">
+            <Image
+              src="/menu_item_Fresh and fruity.jpg"
+              alt="home"
+              width={450}
+              height={700}
+              className="lg:h-96 lg:w-auto w-full object-cover"
+            />
+          </div>
+          <div className="lg:w-1/4 px-4 py-2 lg:px-2 lg:py-0">
+            <Image
+              src="/menu_item_Kitkat shake.jpg"
+              alt="home"
+              width={450}
+              height={700}
+              className="lg:h-96 lg:w-auto w-full object-cover"
             />
           </div>
         </div>
